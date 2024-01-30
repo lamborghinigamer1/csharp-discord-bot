b8dfe2c4a12f05910353e872891f15ffa8f0b99a
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2024-01-30 18:58:54 +0100

    Made Credentials mandatory

--- DiscordBot/Credentials.cs
+++ DiscordBot/Credentials.cs
@@ -2,7 +2,7 @@ namespace DiscordBot;
 
 public struct Credentials()
 {
-    public string Token { set; get; } // The token of the bot
+    public required string Token { set; get; } // The token of the bot
 
-    public ulong GuildId { set; get; } // Positive long for the Discord server
+    public required ulong GuildId { set; get; } // Positive long for the Discord server
 }
